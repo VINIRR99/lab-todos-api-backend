354905efe312e65a65a779692152046b5a08c3fa
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-12 14:25:08 -0400

    post new todo

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -6,9 +6,16 @@ const router = Router();
 
 router.get("/", async (req, res) => {
     try {
-      const allTodos = await Todo.find();
-      res.status(200).json(allTodos);
+        const allTodos = await Todo.find();
+        res.status(200).json(allTodos);
     } catch (error) {res.status(500).json({ error: error.message })}
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const newTodo = await Todo.create(req.body);
+        res.status(201).json(newTodo);
+    } catch (error) {res.status(500).json({ error: error.message })};
 });
 
 module.exports = router;
