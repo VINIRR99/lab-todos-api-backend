89953af28c19eb9ec52f06542798ffbfba24df5a
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-12 10:59:46 -0400

    timestamps

--- models/Todo.js
+++ models/Todo.js
@@ -4,7 +4,8 @@ const todoSchema = new Schema(
     {
         title: { type: String, required: true },
         completed: { type: Boolean, default: false }
-    }
+    },
+    { timestamps: true }
 );
 
 module.exports = model("Todo", todoSchema);
