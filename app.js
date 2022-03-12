15c0ce41fc75f69a3ea9235300c4fb8d97a2446e
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-12 13:33:13 -0400

    Connected to the server using express

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 require("dotenv").config();
 
-const connectDb = require('./config/db.config');
+const connectDb = require("./config/db.config");
 connectDb();
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`));
