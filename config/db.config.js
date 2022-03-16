a7fb68e481f1bf3dd92d0738f2a20c8f25baf438
VINIRR99 <85133953+VINIRR99@users.noreply.github.com>
2022-03-16 17:05:22 -0400

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,7 @@ const connectDb = async () => {
     try {
         const { connections } = await connect(process.env.MONGODB_URI);
         console.log(`Connected to the database: ${connections[0].name}`);
-    } catch (error) {console.log("Error connecting to the database", error)};
+    } catch (error) {console.error("Error connecting to the database", error)};
 };
 
 module.exports = connectDb;
