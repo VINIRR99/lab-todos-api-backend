f341597748ba3ba0963c97e20506c85a0af52643
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-17 21:18:39 -0400

    1.2

--- models/Todo.js
+++ models/Todo.js
@@ -2,8 +2,20 @@ const { Schema, model } = require("mongoose");
 
 const todoSchema = new Schema(
     {
-        title: { type: String, required: true },
-        completed: { type: Boolean, default: false }
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
     },
     { timestamps: true }
 );
