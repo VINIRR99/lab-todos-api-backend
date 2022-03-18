23e401ffcb492bd0887963298010cc809178bcdb
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-18 15:01:29 -0400

    2.2 without jwt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,13 +4,13 @@ const User = require("../models/User");
 
 const router = Router();
 
-const { genSalt, hash } = require("bcryptjs");
+const { genSalt, hash, compare } = require("bcryptjs");
 
 router.post("/signup", async (req, res) => {
     const emailError = "Email already used";
 
     try {
-        const { name, email, password } = req.body;
+        const { name, email, password } = await req.body;
 
         const user = await User.findOne({ email });
         if (user) throw new Error(emailError);
@@ -24,6 +24,26 @@ router.post("/signup", async (req, res) => {
         if (error.message === emailError) {
             res.status(409).json({ error: error.message })
         } else res.status(500).json({ error: error.message });
+    };
+});
+
+router.post("/login", async (req, res) => {
+    const incorrectLogin = "Email or Password is invalid";
+
+    try {
+        const { email, password } = await req.body;
+        
+        const user = await User.findOne({ email });
+        if (!user) throw new Error(incorrectLogin);
+
+        const compareHash = await compare(password, user.passwordHash);
+        if (!compareHash) throw new Error(incorrectLogin);
+
+        res.status(200).json({ message: `User "${user.name}" loggedIn` });
+    } catch (error) {
+        if (error.message === incorrectLogin) {
+            res.status(404).json({ error: error.message })
+        } else res.status(401).json({ error: error.message });
     };
 });
 
